0781f3c99a17d1092873c66c02293e0346af5003
emmaguo13 <emma@proofofalpha.io>
2023-03-30 18:10:37 +0700

    inputs generated :)

--- test/generate_inputs.ts
+++ test/generate_inputs.ts
@@ -3,6 +3,8 @@ import { COSEPublicKeyEC2, COSEPublicKey, COSEKEYS } from "./cose";
 import { AsnParser } from '@peculiar/asn1-schema';
 import { ECDSASigValue } from '@peculiar/asn1-ecc';
 
+import * as fs from "fs";
+
 /*
   signal input pubkey[2][k]; 
   signal input r[k];
@@ -14,92 +16,6 @@ import { ECDSASigValue } from '@peculiar/asn1-ecc';
   signal input challenge_offset; // where the challenge is in the json, C.challenge
   signal input authenticator_data[max_auth_data]; // 37 bytes or more, just do an array of 37*8 bits, decoded
 */
-
-// export async function getCircuitInputs(
-//     pubkey,
-//     r, 
-//     s,
-//     challenge, 
-//     client_data_json, 
-//     authenticator_data
-//   ) {
-//     console.log("Starting processing of inputs");
-//     // Derive modulus from signature
-//     // const modulusBigInt = bytesToBigInt(pubKeyParts[2]);
-//     const modulusBigInt = rsa_modulus;
-//     // Message is the email header with the body hash
-//     const prehash_message_string = msg;
-//     // const baseMessageBigInt = AAYUSH_PREHASH_MESSAGE_INT; // bytesToBigInt(stringToBytes(message)) ||
-//     // const postShaBigint = AAYUSH_POSTHASH_MESSAGE_PADDED_INT;
-//     const signatureBigInt = rsa_signature;
-
-//     // Perform conversions
-//     const prehashBytesUnpadded =
-//       typeof prehash_message_string == "string"
-//         ? new TextEncoder().encode(prehash_message_string)
-//         : Uint8Array.from(prehash_message_string);
-//     const postShaBigintUnpadded =
-//       bytesToBigInt(
-//         stringToBytes((await shaHash(prehashBytesUnpadded)).toString())
-//       ) % CIRCOM_FIELD_MODULUS;
-
-//     // Sha add padding
-//     const [messagePadded, messagePaddedLen] = await sha256Pad(
-//       prehashBytesUnpadded,
-//       MAX_HEADER_PADDED_BYTES
-//     );
-
-//     // Ensure SHA manual unpadded is running the correct function
-//     const shaOut = await partialSha(messagePadded, messagePaddedLen);
-//     assert(
-//       (await Uint8ArrayToString(shaOut)) ===
-//         (await Uint8ArrayToString(
-//           Uint8Array.from(await shaHash(prehashBytesUnpadded))
-//         )),
-//       "SHA256 calculation did not match!"
-//     );
-
-//     // Compute identity revealer
-//     let circuitInputs;
-//     const modulus = toCircomBigIntBytes(modulusBigInt);
-//     const signature = toCircomBigIntBytes(signatureBigInt);
-
-//     const message_padded_bytes = messagePaddedLen.toString();
-//     const message = await Uint8ArrayToCharArray(messagePadded); // Packed into 1 byte signals
-//     const base_message = toCircomBigIntBytes(postShaBigintUnpadded);
-
-//     const address = bytesToBigInt(fromHex(eth_address)).toString();
-//     const address_plus_one = (
-//       bytesToBigInt(fromHex(eth_address)) + 1n
-//     ).toString();
-
-//     if (circuit === CircuitType.RSA) {
-//       circuitInputs = {
-//         modulus,
-//         signature,
-//         base_message,
-//       };
-//     } else if (circuit === CircuitType.JWT) {
-//       circuitInputs = {
-//         message,
-//         modulus,
-//         signature,
-//         message_padded_bytes,
-//         address,
-//         address_plus_one,
-//       };
-//     } else {
-//       assert(circuit === CircuitType.SHA, "Invalid circuit type");
-//       circuitInputs = {
-//         m,
-//         m_padded_bytes,
-//       };
-//     }
-//     return {
-//       circuitInputs,
-//       valid: {},
-//     };
-//   }
 
 type PublicKey = {
   x: Uint8Array, 
@@ -115,6 +31,138 @@ const encoder = new cborx.Encoder({
   mapsAsObjects: false,
   tagUint8Array: false,
 });
+
+// Works only on 32 bit sha text lengths
+function int32toBytes(num: number): Uint8Array {
+  let arr = new ArrayBuffer(4); // an Int32 takes 4 bytes
+  let view = new DataView(arr);
+  view.setUint32(0, num, false); // byteOffset = 0; litteEndian = false
+  return new Uint8Array(arr);
+}
+
+function int8toBytes(num: number): Uint8Array {
+  let arr = new ArrayBuffer(1); // an Int8 takes 4 bytes
+  let view = new DataView(arr);
+  view.setUint8(0, num); // byteOffset = 0; litteEndian = false
+  return new Uint8Array(arr);
+}
+
+function mergeUInt8Arrays(a1: Uint8Array, a2: Uint8Array): Uint8Array {
+  // sum of individual array lengths
+  var mergedArray = new Uint8Array(a1.length + a2.length);
+  mergedArray.set(a1);
+  mergedArray.set(a2, a1.length);
+  return mergedArray;
+}
+
+function assert(cond: boolean, errorMessage: string) {
+  if (!cond) {
+    throw new Error(errorMessage);
+  }
+}
+
+// Puts an end selector, a bunch of 0s, then the length, then fill the rest with 0s.
+async function sha256Pad(
+  prehash_prepad_m: Uint8Array,
+  maxShaBytes: number
+): Promise<[Uint8Array, number]> {
+  let length_bits = prehash_prepad_m.length * 8; // bytes to bits
+  let length_in_bytes = int32toBytes(length_bits);
+  prehash_prepad_m = mergeUInt8Arrays(prehash_prepad_m, int8toBytes(2 ** 7));
+  while (
+    (prehash_prepad_m.length * 8 + length_in_bytes.length * 8) % 512 !==
+    0
+  ) {
+    prehash_prepad_m = mergeUInt8Arrays(prehash_prepad_m, int8toBytes(0));
+  }
+  prehash_prepad_m = mergeUInt8Arrays(prehash_prepad_m, length_in_bytes);
+  assert(
+    (prehash_prepad_m.length * 8) % 512 === 0,
+    "Padding did not complete properly!"
+  );
+  let messageLen = prehash_prepad_m.length;
+  while (prehash_prepad_m.length < maxShaBytes) {
+    prehash_prepad_m = mergeUInt8Arrays(prehash_prepad_m, int32toBytes(0));
+  }
+  assert(
+    prehash_prepad_m.length === maxShaBytes,
+    "Padding to max length did not complete properly!"
+  );
+
+  return [prehash_prepad_m, messageLen];
+}
+
+async function Uint8ArrayToCharArray(a: Uint8Array): Promise<string[]> {
+  return Array.from(a).map((x) => x.toString());
+}
+
+const MAX_CHALLENGE = 100
+const MAX_CLIENT_JSON = 512
+const MAX_AUTH_DATA = 512
+
+async function getCircuitInputs(
+  sig: UnwrappedEC2Sig,
+  publicKey: PublicKey,
+  challengeInput: string,
+  challengeOffset: Number,
+  clientDataJson: Uint8Array,
+  authenticatorData: string[]
+  ) {
+    console.log("Starting processing of inputs");
+    console.log(authenticatorData.toString())
+    const hi = new TextEncoder().encode(authenticatorData.toString())
+    console.log("hi")
+    console.log(hi.length)
+
+    // PAD client_data_json
+    const [clientDataPadded, clientDataPaddedLen] = await sha256Pad(
+      clientDataJson,
+      MAX_CLIENT_JSON
+    );
+
+    // PAD challenge 
+    const [challengePadded, challengePaddedLen] = await sha256Pad(
+      new TextEncoder().encode(challengeInput),
+      MAX_CHALLENGE
+    );
+
+    // PAD authenticator_data
+    const [authDataPadded, authDataPaddedLen] = await sha256Pad(
+      new TextEncoder().encode(authenticatorData.join("")), 
+      MAX_AUTH_DATA
+    )
+
+    // Compute identity revealer
+    let circuitInputs;
+
+    const challenge = await Uint8ArrayToCharArray(challengePadded); 
+    const authenticator_data = await Uint8ArrayToCharArray(authDataPadded); 
+    const client_data_json = await Uint8ArrayToCharArray(clientDataPadded); 
+    
+    const r = await Uint8ArrayToCharArray(sig.r);
+    const s = await Uint8ArrayToCharArray(sig.s);
+
+    const xCircom = await Uint8ArrayToCharArray(publicKey.x);
+    const yCircom = await Uint8ArrayToCharArray(publicKey.y);
+
+    const pubkey = new Array(2)
+    pubkey[0] = xCircom
+    pubkey[1] = yCircom
+
+    const challenge_offset = challengeOffset.toString();
+
+    circuitInputs = {
+      pubkey,
+      r,
+      s,
+      challenge,
+      client_data_json,
+      challenge_offset, 
+      authenticator_data
+
+    }
+    return circuitInputs;
+  }
 
 /**  Pass in Uint8Array public key! Returns a COSEPublicKey **/
 function decodeCredentialPublicKey(publicKey: Uint8Array) : COSEPublicKeyEC2 {
@@ -226,7 +274,9 @@ export async function generate_inputs() {
   console.log("decoded client data");
   console.log(client_data);
 
-  // TODO: get challenge offset
+  // challenge offset
+  let challenge_index = client_data.toString().indexOf("challenge") + 12
+
 
   /* AUTHENTICATOR DATA */
   const auth_data = "SZYN5YgOjGh0NBcPZHZgW4_krrmihjLHmVzzuoMdl2MFAAAAAA"
@@ -245,6 +295,8 @@ export async function generate_inputs() {
 
   console.log("binary auth data")
   console.log(auth_data_bin)
+
+  return getCircuitInputs(sig, pubkey, challenge, challenge_index, client_data, auth_data_bin);
 }
 
 async function do_generate() {
@@ -256,9 +308,8 @@ async function do_generate() {
 if (typeof require !== "undefined" && require.main === module) {
   // debug_file();
   const circuitInputs = do_generate().then((res) => {
-    // console.log("Writing to file...");
-    // console.log(res)
-    // fs.writeFileSync(`./jwt.json`, JSON.stringify(res), { flag: "w" });
+    console.log("Writing to file...");
+    fs.writeFileSync(`./webauthn.json`, JSON.stringify(res), { flag: "w" });
   }
   );
   // gen_test();
