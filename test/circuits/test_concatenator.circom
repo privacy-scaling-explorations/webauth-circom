eae96a477ef2fd4dfee509664e74968d0abe6c49
SleepingShell <SleepingShell@users.noreply.github.com>
2023-12-21 15:09:01 -0700

    Cleanup and test concatenator circuit

---
+++ test/circuits/test_concatenator.circom
@@ -0,0 +1,5 @@
+pragma circom 2.1.5;
+
+include "../../circuits/utils.circom";
+
+component main = concatenate_arrays(8,16,24);
---
+++ test/utils.ts
@@ -0,0 +1,34 @@
+export function bufferToBitArray(b: Buffer) {
+  const res = [];
+  for (let i=0; i<b.length; i++) {
+      for (let j=0; j<8; j++) {
+          res.push((b[i] >> (7-j) &1));
+      }
+  }
+  return res;
+}
+
+export function bitArrayTobuffer(a: number[] | bigint[]) {
+  const len = Math.floor((a.length -1 )/8)+1;
+  const b = Buffer.alloc(len);
+
+  for (let i=0; i<a.length; i++) {
+      const p = Math.floor(i/8);
+      b[p] = b[p] | (Number(a[i]) << ( 7 - (i%8)  ));
+  }
+  return b;
+}
+
+export function bufferToBigIntArray(arr: Buffer): bigint[] {
+  let res: bigint[] = [];
+  arr.forEach((x) => res.push(BigInt(x)));
+
+  return res;
+}
+
+export function BigIntArrayToBuffer(arr: bigint[]): Buffer {
+  let res =  Buffer.alloc(arr.length);
+  arr.forEach((x, i) => res[i] = Number(x));
+
+  return res;
+}
